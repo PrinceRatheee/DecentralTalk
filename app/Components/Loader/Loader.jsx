952055435d6c61d1d18ec6e6d3aab3c9e1686838
PrinceRatheee <rathi.prince2@gmail.com>
2024-10-11 10:52:19 +0530

    Loader done

--- app/Components/Loader/Loader.jsx
+++ app/Components/Loader/Loader.jsx
@@ -1,8 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
+
+//INTERNAL IMPORT
+import Style from "./Loader.modulo.css";
+import images from "../../assets";
+
 
 function Loader() {
   return (
-    <div>Loader</div>
+    <div className={Style.Loader}>
+      <div className={Style.Loader_box}>
+        <Image src={images.loader} alt='loader' width={100} height={100} />
+      </div>
+    </div>
   )
 }
 
