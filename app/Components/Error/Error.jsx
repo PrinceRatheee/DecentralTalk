8898e47eff3d2132dd0e156c8592107e82733434
PrinceRatheee <rathi.prince2@gmail.com>
2024-10-11 10:59:39 +0530

    Error component done

--- app/Components/Error/Error.jsx
+++ app/Components/Error/Error.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const Error = () => {
+// INTERNAL IMPORT 
+import Style from "./Error.modulo.css";
+
+
+const Error = ({error}) => {
   return (
-    <div>Error</div>
+    <div className={Style.Error}>
+      <div className={Style.Error_box}>
+        <h1>Please fix this error and reload browser</h1>
+        {error}
+      </div>
+    </div>
   )
 }
 
